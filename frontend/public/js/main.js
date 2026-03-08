728cae72ad3632a779829ae115f9fab860fb5f80
andrs-beepboop <solar2005@libero.it>
2026-03-08 12:27:09 +0100

    configurazione per localhost main.js

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost/esercizioSlim/backend/api';
+const API_BASE_URL = 'http://localhost/esercizioSlim/backend/api/v2';
 const DEFAULT_LIMIT = 10;
 
 const dictionary = {
